c76e1e2adaa00b3315a8f4498b5194095541eea0
Gabriel Henrique <ghsb11@gmail.com>
2024-01-19 11:04:54 -0300

    modal implemetado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImage from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTrasactionModal";
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImage} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    <NewTransactionModal />
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
---
+++ src/components/NewTrasactionModal/index.tsx
@@ -0,0 +1,27 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay } from "./styles";
+import { X } from "phosphor-react";
+
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal >
+            <Overlay>
+                <Content>
+                    <Dialog.Title>Nova transação</Dialog.Title>
+                    <CloseButton > <X size={24} /> </CloseButton>
+                    <form action="">
+                        <input type="text" placeholder="Descrição" required/>
+                        <input type="number" placeholder="Preço" required/>
+                        <input type="number" placeholder="Categoria" required/>
+
+                        <button type="submit">
+                            Cadastrar
+                        </button>
+                    </form>
+                    
+                </Content>
+            </Overlay>
+        </Dialog.Portal>
+    )
+}
